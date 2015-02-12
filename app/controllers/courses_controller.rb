0882b1d93c0ce3e4aca6875120c5a542ed5ba88d
taocv56 <chutao1993@gmail.com>
2015-02-12 15:15:04 +0700

    create course model and its controller
    test

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,50 @@
+class CoursesController < ApplicationController
+
+  def show
+    @course = Course.find params[:id]   
+  end
+
+  def new
+    @course = Course.new
+  end
+
+  def index
+    @courses = Course.all
+  end
+
+  def destroy
+    Course.find(params[:id]).destroy
+    flash[:success] = "Course deleted"
+    redirect_to courses_url
+  end
+
+  def create
+    @course = Course.new course_params
+    if @course.save      
+      flash[:info] = "Successful!"
+      redirect_to courses_url
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @course = Course.find params[:id]
+  end
+
+  def update
+    @course = Course.find params[:id]
+    if @course.update_attributes course_params
+      flash[:success] = "Successful!"
+      redirect_to @course
+    else
+      render 'edit'
+    end
+  end
+  
+  private
+    def course_params
+      params.require(:course).permit(:name, :content)
+    end
+end
+
---
+++ app/models/course.rb
@@ -0,0 +1,3 @@
+class Course < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
+end
